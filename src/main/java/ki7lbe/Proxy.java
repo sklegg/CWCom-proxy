ea3698c653fe643f9116671576de502be133c5ef
Scott Legg <sklegg@gmail.com>
2020-02-15 14:49:01 -0800

    initial commit

---
+++ src/main/java/ki7lbe/Proxy.java
@@ -0,0 +1,57 @@
+package ki7lbe;
+
+import org.netcrusher.core.filter.LoggingFilter;
+import org.netcrusher.core.reactor.NioReactor;
+import org.netcrusher.datagram.DatagramCrusher;
+import org.netcrusher.datagram.DatagramCrusherBuilder;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Proxy {
+
+    public static void main(String[] args) {
+        logParams(args);
+
+        InetSocketAddress localAddress = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
+        InetSocketAddress remoteAddress = new InetSocketAddress(args[2], Integer.parseInt(args[3]));
+
+        NioReactor reactor;
+
+        try {
+            reactor = new NioReactor();
+        } catch (IOException exception) {
+            return;
+        }
+        DatagramCrusher crusher = DatagramCrusherBuilder.builder()
+                .withReactor(reactor)
+                .withBindAddress(localAddress)
+                .withConnectAddress(remoteAddress)
+                .withIncomingTransformFilterFactory((addr) -> new LoggingFilter(localAddress, "client", LoggingFilter.Level.DEBUG))
+                .withOutgoingTransformFilterFactory((addr) -> new LoggingFilter(remoteAddress, "server", LoggingFilter.Level.DEBUG))
+                .buildAndOpen();
+
+        /* https://github.com/NetCrusherOrg/netcrusher-java/blob/master/samples/sample-datagram-rfc868/src/test/java/org/netcrusher/DatagramCrusherRFC868Test.java */
+
+        //while (crusher.isOpen()) {
+            System.out.println("crusher");
+        //}
+
+        System.out.println("crusher closed");
+        crusher.close();
+        reactor.close();
+    }
+
+    private static void logParams(String[] params) {
+        // TODO: use logger instead
+        System.out.print("-- IN --");
+        System.out.print("host: " + params[0] + '\t');
+        System.out.print("port: " + params[1]);
+
+        System.out.println();
+        System.out.print("-- OUT --");
+        System.out.print("host: " + params[2] + '\t');
+        System.out.print("port: " + params[3]);
+        System.out.println();
+    }
+}
---
+++ src/main/java/ki7lbe/filters/InFilter.java
@@ -0,0 +1,12 @@
+package ki7lbe.filters;
+
+import org.netcrusher.core.filter.TransformFilter;
+
+import java.nio.ByteBuffer;
+/* https://github.com/NetCrusherOrg/netcrusher-java/blob/master/core/src/main/java/org/netcrusher/core/filter/TransformFilter.java */
+public class InFilter implements TransformFilter {
+    @Override
+    public void transform(ByteBuffer byteBuffer) {
+
+    }
+}
